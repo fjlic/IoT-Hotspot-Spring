369d3fb01662e48aa95f6d52e106322b941d965f
fjlic <franc.javier.flores@gmail.com>
2022-04-08 23:02:47 -0500

    My first commit with Sprin-MVC-Boot and Maven

---
+++ src/main/java/com/example/IoTHotspot/IoTHotspotApplication.java
@@ -0,0 +1,13 @@
+package com.example.IoTHotspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTHotspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTHotspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IoTHotspot/IoTHotspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IoTHotspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTHotspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
