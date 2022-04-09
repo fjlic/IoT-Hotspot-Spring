d2f176c0a9f3f9597c85890e33e3dac9b3530bb3
fjlic <franc.javier.flores@gmail.com>
2022-04-08 23:28:55 -0500

    Load Template Admin-LTE in File Static for the construct Front-End

---
+++ src/main/java/com/example/IoTHotspot/Controllers/UserController.java
@@ -0,0 +1,71 @@
+package com.example.IoTHotspot.Controllers;
+
+import com.example.IoTHotspot.Models.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "index")
+    public User index() {
+        User user = new User();
+        user.setId(10);
+        user.setName("User Test");
+        user.setEmail("test@domain.com");
+        user.setPassword("abc123");
+        user.setConfirm_password("abc123");
+        user.setToken("#ABC123@321CBA#");
+        return user;
+    }
+
+    @RequestMapping(value = "create")
+    public User create() {
+        User user = new User();
+        user.setId(10);
+        user.setName("User Test");
+        user.setEmail("test@domain.com");
+        user.setPassword("abc123");
+        user.setConfirm_password("abc123");
+        user.setToken("#ABC123@321CBA#");
+        return user;
+    }
+
+    @RequestMapping(value = "show/{id}")
+    public User show(@PathVariable int id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("User Test");
+        user.setEmail("test@domain.com");
+        user.setPassword("abc123");
+        user.setConfirm_password("abc123");
+        user.setToken("#ABC123@321CBA#");
+        return user;
+    }
+
+    @RequestMapping(value = "edit/{id}")
+    public User edit() {
+        User user = new User();
+        user.setId(10);
+        user.setName("User Test");
+        user.setEmail("test@domain.com");
+        user.setPassword("abc123");
+        user.setConfirm_password("abc123");
+        user.setToken("#ABC123@321CBA#");
+        return user;
+    }
+
+    @RequestMapping(value = "delete/{id}")
+    public User delete() {
+        User user = new User();
+        user.setId(10);
+        user.setName("User Test");
+        user.setEmail("test@domain.com");
+        user.setPassword("abc123");
+        user.setConfirm_password("abc123");
+        user.setToken("#ABC123@321CBA#");
+        return user;
+    }
+    
+}
---
+++ src/main/java/com/example/IoTHotspot/Models/User.java
@@ -0,0 +1,60 @@
+package com.example.IoTHotspot.Models;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private String confirm_password;
+    private String token;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    } 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirm_password() {
+        return confirm_password;
+    }
+
+    public void setConfirm_password(String confirm_password) {
+        this.confirm_password = confirm_password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
+}
